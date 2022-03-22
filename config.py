7aaff2e0732968bee6954d95402311defa723981
EuiseokJeong <dmltjr4621@gmail.com>
2022-03-22 12:17:31 +0900

    AMP Done

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ configs = {
     'stride':10,
     'time_window':2,
     'data_path': '/home/euiseokjeong/Desktop/IMLAB/2022_ABAW_imlab/data/2022',
-    'limit_gpu':1024*1,
-    'gpu_num': 0,
+    'limit_gpu':1024*5,
+    'gpu_num': 1,
     'eval_path': '/home/euiseokjeong/Desktop/IMLAB/2022_ABAW_imlab/NAS/2022/result/2022_3_20_2_30_59',
 
     # train
@@ -12,10 +12,10 @@ configs = {
     'batch_size':256,
     'early_stop': 5,
     'generation': 5,
-    'learning_rate': 0.0001,
+    'learning_rate': 0.00001,
 
     # model
-    'feature_extractor_layers':[1024,512],
+    'feature_extractor_layers':[1024,1024,512,512],
     'classifier_layers': [256,128],
     'domain_layers': [256, 128],
     'lstm_num': 512,
@@ -33,8 +33,7 @@ configs = {
     'task_weight_flag':True,
 
      # domain adaptation
-    'domain_weight':0,
-    'adaptation_factor':0,
+    'domain_weight':1,
+    'adaptation_factor':1,
     'exp_domain_weight':False
-    'AMP':0
 }
